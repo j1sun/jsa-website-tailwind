92837a889d0cc41ec249ab760a53fba27e90dbb7
Justin Sun <justinsun101@gmail.com>
2019-09-18 16:09:16 -0700

    Refactored redesign into gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
-var postcss = require('gulp-postcss');
+var postcss = require('gulp-html-postcss');
 var gulp = require('gulp');
+var cssImport = require('postcss-import');
 var tailwindcss = require('tailwindcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 
 gulp.task('css', function () {
   var plugins = [
+    cssImport,
     tailwindcss,
     autoprefixer,
     cssnano
   ];
-  return gulp.src('./src/*.css')
+
+  gulp.src('./src/*.html')
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest('./dest'));
+
+  gulp.src('./src/img/*')
+    .pipe(gulp.dest('./dest/img/'));
+
+  return gulp.src('./src/style.css')
     .pipe(postcss(plugins))
     .pipe(gulp.dest('./dest'));
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 module.exports = {
   theme: {
-    extend: {}
+    fontFamily: {
+      'Noto-Sans-JP': ['"Noto Sans JP"'],
+      'Montserrat': ['Montserrat'],
+    },
+    extend: {
+      colors: {
+        bg: {
+          light: '#f7f4f7',
+          dark: '#221722',
+        },
+        brand: {
+          light: '#f46c57',
+          default: '#f03214',
+          dark: '#b53c50',
+        }
+      },
+    },
   },
-  variants: {},
+  variants: {
+  },
   plugins: []
 }
